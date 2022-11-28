d82d96f0479efcd9e50386c821505177a24b989e
John Henry Niño Pico <jhonnino@unisangil.edu.co>
2022-11-28 10:11:16 -0500

    remain time ok

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -3,11 +3,11 @@ import { FaRegClock, FaRegStar, FaStar } from "react-icons/fa";
 
 const Product: React.FC<{
   className?: string;
-  productName?: string;
-  productPrice?: number;
-  productImg?: string;
-  productCalification?: number;
-  productDateOfExpiration?: Date;
+  productName: string;
+  productPrice: number;
+  productImg: string;
+  productCalification: number;
+  productDateOfExpiration: Date;
 }> = ({
   productName,
   productPrice,
@@ -21,10 +21,49 @@ const Product: React.FC<{
     setToggle(!toggle);
     console.log(toggle);
   };
+
+  const stars: JSX.Element[] = [];
+
+  for (let index = 1; index <= 5; index++) {
+    productCalification >= index
+      ? stars.push(
+          <FaStar className="star-active" key={`star-active ${index}`} />
+        )
+      : stars.push(
+          <FaRegStar
+            className="star-unactivate"
+            key={`star-unactivate ${index}`}
+          />
+        );
+  }
+
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const getRemainTime = (deadline: Date) => {
+    let now = Date.now();
+    let parseDeadLine = Date.parse(deadline.toString());
+    let remainTime = (parseDeadLine - now + 1000) / 1000;
+    let remainSeconds = ("0" + Math.floor(remainTime % 60)).slice(-2);
+    let remainMinutes = ("0" + Math.floor((remainTime / 60) % 60)).slice(-2);
+    let remainHours = ("0" + Math.floor((remainTime / 3600) % 24)).slice(-2);
+    let remainDays = Math.floor(remainTime / (3600 * 24));
+    return {
+      remainTime,
+      remainDays,
+      remainHours,
+      remainMinutes,
+      remainSeconds,
+    };
+  };
+
+  const remainTime = getRemainTime(productDateOfExpiration);
+
   return (
     <div className="product">
       <div className="product__box-top-image">
-        {/* <div style={{ backgroundImage: `${productImg}` }}></div> */}
         <img src={productImg} alt={productImg} />
       </div>
       <div className="product__box-details">
@@ -34,23 +73,19 @@ const Product: React.FC<{
             <button onClick={handleToggle}>፧</button>
           </div>
           <span className="product__box-details-container-price">
-            $ {productPrice}
+            {formatter.format(productPrice)}
           </span>
           <div className="product__box-details-container-timestamp-calification">
             <div className="product__box-details-container-timestamp-calification-timestamp">
               <FaRegClock />
-              <span>{productDateOfExpiration?.toString()}</span>
+              <span>
+                {remainTime.remainTime > 0
+                  ? `${remainTime.remainDays} d ${remainTime.remainHours} h`
+                  : "Expired"}
+              </span>
             </div>
             <div className="product__box-details-container-timestamp-calification-calification">
-              {/* {for (let index = 0; index < productCalification; index++) {
-                const element = array[index];
-                
-              }} */}
-              <FaStar className="star-active" />
-              <FaStar className="star-active" />
-              <FaRegStar className="star-unactivate" />
-              <FaRegStar className="star-unactivate" />
-              <FaRegStar className="star-unactivate" />
+              {stars.map((star) => star)}
             </div>
           </div>
         </div>
